68281ccf90d7fdcec85db6a1f86c01e2ec0b52b3
Choooooper <126206660+Choooooper@users.noreply.github.com>
2024-03-06 14:16:01 +1100

    game

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControls : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+     public float speed = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnMove(InputValue movementValue) 
+    {
+        Vector2 movementVector = movementValue.Get<Vector2> ();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+
+    }
+
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement);
+
+        rb.AddForce(movement * speed); 
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        
+    }
+}
